1a7e5c47f848e09f64221292d69fd6392cf3863e
michaelRecognite <michaelRecognite.com.au>
2019-06-02 21:34:54 +1000

    Initial Commit

---
+++ ParallaxStickyHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxStickyHeader
+//
+//  Created by mk mk on 2/6/19.
+//  Copyright © 2019 MC2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
